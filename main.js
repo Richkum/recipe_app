2f2655bd33e88c5d04fe4584eb172597a0229107
Richkum <kumatum2@gmail.com>
2023-10-18 13:08:41 +0100

    doing my js

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-import './style.css'
-import { setupCounter } from './counter.js'
+const latest = document.querySelector('.latest')
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
-document.querySelector('#app').innerHTML = 
+async function getapi(url) {
 
+    const response = await fetch(url);
 
-setupCounter(document.querySelector('#counter'))
+    let data = await response.json();
+    console.log(data);
+    let infos = data.meals
+    latest.innerHTML = infos.map((food) => {
+   return `<div class="late">
+      <img class="recip-img" src="${food.strMealThumb}" alt="">
+      <h4>${food.strMeal}</h4>
+    </div>`
+    })
+    console.log(infos)
+}
+getapi(apiUrl)
+
+// async function getMeals(name) {
+// const data = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name || ''}`).then(res=>res.json()).then()
+// return data
+// }
+// // 
