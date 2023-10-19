43443c91aefa808831dba3921fe374fa28ad29d0
Richkum <kumatum2@gmail.com>
2023-10-19 09:04:38 +0100

    doing my js

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 const latest = document.querySelector('.latest')
-const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+const popular = document.querySelector('.popular')
+console.log(popular)
+
+const urlApi = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 
 async function getapi(url) {
 
-    const response = await fetch(url);
+  const response = await fetch(url);
+  let data = await response.json();
+  let infos = data.categories
+  infos.forEach((food) => {
+    popular.innerHTML += `<div class="pop1">
+    <img class="pop-img" src="${food.strCategoryThumb}" alt="">
+    <h3 class="pasta">${food.strCategory}</h3>
+  </div>
+    `
+  })
 
-    let data = await response.json();
+}
+getapi(urlApi)
+
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+async function getapi3(url3) {
+
+    const response3 = await fetch(url3);
+
+    let data = await response3.json();
     console.log(data);
     let infos = data.meals
-    latest.innerHTML = infos.map((food) => {
-   return `<div class="late">
+    infos.forEach((food) => {
+      latest.innerHTML += `<div class="late">
       <img class="recip-img" src="${food.strMealThumb}" alt="">
       <h4>${food.strMeal}</h4>
     </div>`
     })
     console.log(infos)
 }
-getapi(apiUrl)
+getapi3(apiUrl)
+
 
 // async function getMeals(name) {
 // const data = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name || ''}`).then(res=>res.json()).then()
